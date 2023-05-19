9d40a4c38c2b2b41bd397f577590346501af6f16
Anish Pokharel <anish.pokharel.710.ap@gmail.com>
2023-05-19 15:11:37 +0545

    code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+// import * as alertify from 'alertifyjs'
 
 @Component({
   selector: 'app-root',
@@ -7,18 +8,104 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AppComponent implements OnInit {
   // title = 'relearn';
+  // states: Array<String> = ['kathmandu', 'lalitpur ', 'bhaktapur'];
+  // user = { 'fname': 'anish', 'lname': 'pokharel' };
+  // isEdit: boolean = false;
+  // students = [{
+  //   Fullname: 'asdfasdf',
+  //   Email: 'test@gmail.com',
+  //   ContactNo: '9879877',
+  //   IsPaid: true,
+  //   isEdit: false
+  // }, {
+  //   Fullname: '324234',
+  //   Email: 'data1@gmail.com',
+  //   ContactNo: '567567',
+  //   IsPaid: true,
+  //   isEdit: false
+  // }, {
+  //   Fullname: 'hjkl',
+  //   Email: 'stdf2@gmail.com',
+  //   ContactNo: '123123',
+  //   IsPaid: true,
+  //   isEdit: false
+  // }]
+
+
+  // nayaStudent = {
+  //   Fullname: '',
+  //   Email: '',
+  //   ContactNo: '',
+  //   IsPaid: false,
+  //   isEdit: true
+
+  // }
+  // // people: string[] = []
+  // // nameho!: string;
+  // // phoneho!: string;
+  // // emailho!: string;
+
+
+
+  // variable user and some default values
   constructor() { }
+
+
 
   ngOnInit() {
 
   }
 
 
-  // calling it with method and passing the myForm as Parameters
-  submitHandler(myForm: any) {
-    console.log(myForm)
-  }
+  //   // calling it with method and passing the myForm as Parameters
+  //   submitHandler(myForm: any) {
+  //     console.log(myForm);
+
+  //     alertify.alert(" you clicked")
+  //     // console.log(this.user);
+  //     // console.log("Form value is ", myForm.value)
+  //   }
+  //   addDo() {
+  //     this.students.push(this.nayaStudent);
+  //     this.nayaStudent = {
+  //       Fullname: '',
+  //       Email: '',
+  //       ContactNo: '',
+  //       IsPaid: false,
+  //       isEdit: false
+  //     }
+  //   }
+
+  //   // editStudent(student: any) {
+  //   //   if (student.isEdited) {
+  //   //     alertify.alert('cannot be edited again ');
+  //   //     return;
+  //   //   }
+  //   //   alertify.confirm('are you sure to edit ', () => {
+  //   //     student.isEdited = true;
+  //   //     this.nayaStudent = student;
+  //   //   }
+  //   //   )}
+
+
+  //   editStudent(student: any,) {
+  //     student.isEdit = true;
+
+
+
+  //   }
+
+  //   deleteStudent(index: number) {
+  //     alertify.confirm('Are You sure to delete', () => {
+  //       this.students.splice(index, 1);
+  //       alertify.alert("items is deleted");
+
+  //     }, () => {
+  //       alertify.alert('you clicked cancel ')
+  //     });
+
+  //   }
+
+  // }
 
 }
-
-
